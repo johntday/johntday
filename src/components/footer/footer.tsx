b47f51fc18f9ec4ed027062b89662626a30ebfcf
John Day <john.day@mybobs.com>
2025-01-09 17:57:57 -0600

    footer and personal

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -30,20 +30,6 @@ export default function Footer(): ReactNode {
           <div>
             Copyright © {new Date().getFullYear()} {fullName}
           </div>
-          <div className="text-sm">
-            This résumé was generated with{' '}
-            <a className="text-link" href="https://nextjs.org/">
-              Next.js
-            </a>
-            . Learn how to deploy your own résumé with the{' '}
-            <a
-              className="text-link"
-              href="https://github.com/colinhemphill/nextjs-resume"
-            >
-              nextjs-resume
-            </a>
-            !
-          </div>
         </div>
       </div>
     </footer>
