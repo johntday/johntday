26cb046648921ed2ac41f45092110e33e05df114
John Day <john.day@mybobs.com>
2025-01-09 17:39:17 -0600

    main changes

--- edit-me/config/links.ts
+++ edit-me/config/links.ts
@@ -1,36 +1,33 @@
 import { CMSLink } from '@config-types/cms-link';
 import {
-  SiBluesky,
   SiGithub,
-  SiInstagram,
   SiLinkedin,
-  SiNpm,
 } from '@icons-pack/react-simple-icons';
 
 export const links: CMSLink[] = [
   {
-    href: 'https://www.linkedin.com',
+    href: 'https://www.linkedin.com/in/johntday',
     icon: SiLinkedin,
     title: 'LinkedIn',
   },
   {
-    href: 'https://github.com',
+    href: 'https://github.com/johntday',
     icon: SiGithub,
     title: 'GitHub',
   },
-  {
-    href: 'https://www.npmjs.com',
-    icon: SiNpm,
-    title: 'NPM',
-  },
-  {
-    href: 'https://bsky.app',
-    icon: SiBluesky,
-    title: 'Bluesky',
-  },
-  {
-    href: 'https://www.instagram.com',
-    icon: SiInstagram,
-    title: 'Instagram',
-  },
+  // {
+  //   href: 'https://www.npmjs.com',
+  //   icon: SiNpm,
+  //   title: 'NPM',
+  // },
+  // {
+  //   href: 'https://bsky.app',
+  //   icon: SiBluesky,
+  //   title: 'Bluesky',
+  // },
+  // {
+  //   href: 'https://www.instagram.com',
+  //   icon: SiInstagram,
+  //   title: 'Instagram',
+  // },
 ];
